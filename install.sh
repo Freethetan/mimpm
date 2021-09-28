580c1b16c54b927f94a85ed92dc60cc44e0a3509
Viktor Honchar <viktor.gonchar@gmail.com>
2021-09-28 15:13:05 -0400

    Fix installer

--- install.sh
+++ install.sh
@@ -7,6 +7,14 @@ echo "Assuming this project is already located in correct folder
   Add export to your .profile or .bashrc file to load aliases
 "
 mim_app_path=`dirname $0`
+if ! [ "$1"=="--reinstall" ] && [ -x `which mimpm` ];then
+  echo "mimp is already installed
+  To force install use --reinstall key"
+  exit;
+fi
 mim_app_exports="export PATH=$mim_app_path/bin:\$PATH"
 
 echo "$mim_app_exports" > ~/.bashrc
+export PATH=$mim_app_path/bin:$PATH
+
+mimpm --set-editor
